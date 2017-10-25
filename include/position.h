2e5148fafb0384764652a2a913be6d4604f61ada
Clement LUCAS <lucasc.ts1@gmail.com>
2017-10-25 09:05:28 +0200

    first structures and main

---
+++ include/position.h
@@ -0,0 +1,29 @@
+/* -*- c-basic-offset: 3 -*- 
+ *
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin 
+ * F-14050 Caen Cedex 
+ *
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */ 
+
+/**
+ * @author Clément LUCAS
+ * @version 0.0.1 / 25-10-2017
+ */
+
+/**
+ * @file position.h
+ */
+
+#ifndef __POSITION_H
+#define __POSITION_H
+
+typedef struct{
+   int x;
+   int y;
+} Position;
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,41 @@
+/* -*- c-basic-offset: 3 -*- 
+ *
+ * ENSICAEN
+ * 6 Boulevard Marechal Juin 
+ * F-14050 Caen Cedex 
+ *
+ * This file is owned by ENSICAEN students.
+ * No portion of this document may be reproduced, copied
+ * or revised without written permission of the authors.
+ */ 
+
+/**
+ * @author Clément LUCAS
+ * @author Clément RIVIERE
+ * @version 0.0.1 / 25-10-2017
+ */
+
+/**
+ * @file main.c
+ * Main function of the game BattleShip
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void) {
+
+   /* Print the two plates */
+
+   /* Initialize the game*/
+
+   /* Game */
+
+   /* Print the result */
+
+   /* Play again ?*/
+   
+   return EXIT_SUCCESS;
+}
+
